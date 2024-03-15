70d29cc14f860946125e2f3845d9fa7f5ce4c7ce
Martin5587 <mcontreras265@insite.4cd.edu>
2024-03-15 16:56:37 -0700

    commit(1)

--- main.hpp
+++ main.hpp
@@ -1,3 +1,17 @@
-/***************************************************
- * Code your program here
- ***************************************************/
+#ifndef MAIN_HPP
+#define MAIN_HPP
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int writeFile(const string& filename) {
+    ofstream outFile(filename);
+    if (!outFile) {
+        cerr << "ERROR opening file" << endl;
+    }
+
+    int numEmployees = 3
+}
